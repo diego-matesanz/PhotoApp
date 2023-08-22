b45f326c97208e0dbc2cdd0e3d1c25d366680320
Diego Matesanz <diegomatesanzfuente@gmail.com>
2023-08-22 16:00:55 +0200

    Changed API key

--- app/src/main/java/com/deneb/unsplashapp/features/photos/PhotosApi.kt
+++ app/src/main/java/com/deneb/unsplashapp/features/photos/PhotosApi.kt
@@ -2,14 +2,15 @@ package com.deneb.unsplashapp.features.photos
 
 import com.deneb.unsplashapp.features.photos.model.UnsplashResponse
 import com.deneb.unsplashapp.features.photos.model.UnsplashDetailResponse
+import com.deneb.unsplashapp.utils.ConstantsUtil
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 internal interface PhotosApi {
-    @GET("photos/?client_id=TU_CLAVE_AQUÍ")
+    @GET("photos/?client_id=${ConstantsUtil.UNSPLASH_API_KEY}")
     fun photos(): Call<UnsplashResponse>
 
-    @GET("photos/{id}/?client_id=TU_CLAVE_AQUÍ")
+    @GET("photos/{id}/?client_id=${ConstantsUtil.UNSPLASH_API_KEY}")
     fun detailPhoto(@Path("id")id: String): Call<UnsplashDetailResponse>
 }
---
+++ app/src/main/java/com/deneb/unsplashapp/utils/ConstantsUtil.kt
@@ -0,0 +1,6 @@
+package com.deneb.unsplashapp.utils
+
+object ConstantsUtil {
+
+    const val UNSPLASH_API_KEY = "tmr_PqBQovlCcGQgjwvPJPiUGqruqDOs-MMgrXrAgSQ"
+}
